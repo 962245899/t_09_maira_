66520fb70097eb291bd6da44014cd9a2f7b3ac1b
962245899 <57157985+962245899@users.noreply.github.com>
2019-12-20 13:29:49 -0500

    Add files via upload
    sandoval

--- libreria.py
+++ libreria.py
@@ -1,158 +1,152 @@
-#ejercicio01
-def gasto_panetones(pa,p_u):
-    gasto_panetones=pa*p_u
-    return gasto_panetones
-#fin ejercicio01
+#EJERCICIO 01
+def gasto_tortas(to,p_u):
+    gasto_tortas=to*p_u
+    return gasto_tortas
+#fin
+
+#EJERCICIO 02
+def arroz_chaufa(co,tp):
+    arroz_chaufa=tp*co
+    return arroz_chaufa
+#fin
 
 
-#ejercicio02
-def mazamorra_consumida(co,tp):
-    mazamorra_consumida=tp*co
-    return mazamorra_consumida
-#fin ejercicio02
+#EJERCICIO 03
+def notas_examene(notas_examen,cu):
+    notas_examen=notas_examen*cu
+    return notas_examen
+#fin
 
+#EJERCICIO 04
+def area_rectangulo(b,a):
+    area_rectangulo=(a*b)
+    return area_rectangulo
+#fin
 
-#ejercicio03
-def trabajos_realizados(tc,cu):
-    trabajos_realizados=tc*cu
-    return trabajos_realizados
-#fin ejercicio03
-
-
-#ejercioio04
-def area_cuadrado(b,a):
-    area_cuadrado=(a*b)
-    return area_cuadrado
-##fin ejercicio04
-
-
-#ejercicio05
-def menor(a,b):
-    if(a < b):
+#EJERCICIO 05
+def mayor(a,b):
+    if(a > b):
         return a
 
 #fin
 
-#ejercicio06
-def cantidad_mayor(c,f):
-    if(c > f):
-        return c
-#fin ejercicio06
+#EJERCICIO 06
+def cantidad_menor(a,b):
+    if(a<b):
+        return a
+#fin
 
-#ejercicio07
-def gastos_auto(r,c):
-    gastos_auto=r+c
-    return gastos_auto
-#fin ejercicio7
+#EJERCICIO 07
+def gastos_casa(L,A):
+    gastos_casa=L+A
+    return gastos_casa
+#fin
 
-
-#ejercicio08
-def sorteo(p,o):
-    if(p > o):
-        return p
+#EJERCICIO 08
+def sorteo(a,b):
+    if(a > b):
+        return a
     else:
-        return o
-#fin ejercicio08
+        return b
+#fin
 
+#EJERCICIO 09
+def compra_de_autos(ca,u):
+    compra_de_autos=(ca*u)
+    return compra_de_autos
+#fin
 
-#ejercicio09
-def compra_de_caramelos(ca,u):
-    compra_de_caramelos=(ca*u)
-    return compra_de_caramelos
-#fin ejercicio 09
+#EJERCICIO 10
+def total_de_libros(sa,se,si,so):
+    total_de_libros=(sa,se,si,so)
+    return total_de_libros
+#fin
 
-#ejercicio10
-def total_de_paginas(la,le,li):
-    total_de_paginas=(la+le+li)
-    return total_de_paginas
-#fin ejercicio10
+#EJERCICIO 11
+def tratamiento_dientes(co,mes):
+    tratamiento_dientes=(co*mes)
+    return tratamiento_dientes
+#fin
 
-#ejercicio11
-def tratamiento_brackets(co,mes):
-    tratamiento_brackets=(co*mes)
-    return tratamiento_brackets
-#fin ejercicio11
-
-#ejercicio12
+#EJERCICIO 12
 def h_estudio(hd,di):
     h_estudio=(hd*di)
     return h_estudio
-#fin ejercicio12
+#fin
 
-#ejercicio13
-def comidas_entregadas(ra,al,dia):
-    comidas_entregadas=(ra*al*dia)
-    return comidas_entregadas
-#fin ejercicio13
+#EJERCICIO 13
+def numero_jalados(ja,mb):
+    numero_jalados=(ja+mb)
+    return numero_jalados
+#fin
 
-#ejercicio14
-def frasadas_donadas(mes):
-    frasadas_donadas=(mes*12)
-    return frasadas_donadas
-#fin ejercicio14
+#EJERCICIO 14
+def jugetes_donados(mes):
+    jugetes_donados=(mes*12)
+    return jugetes_donados
+#fin
 
-#ejercicio15
-def colecta_chocolatada(ju,du):
-    colecta_chocolatada=(ju+du)
-    return colecta_chocolatada
-#fin ejercicio15
+#EJERCICIO 15
+def recaudacion_dinero(di,pe):
+    recaudacion_dinero=(di+pe)
+    return recaudacion_dinero
+#fin
 
-#ejercicio16
-def mes_vida(an):
-    mes_vida=(an*12)
-    return mes_vida
-#fin ejercicio16
+#EJERCICIO 16
+def peso_bloque(ma,gr):
+    peso_bloque=(ma*gr)
+    return peso_bloque
+#fin
 
-#ejercicio17
-def t_paños(pap,top):
-    t_paños=(pap*top)
-    return t_paños
-#fin ejercicio17
+#EJERCICIO 17
+def fuerza_bloque(ma,ac):
+    fuerza_bloque=(ma*ac)
+    return fuerza_bloque
+#fin
 
-#ejercicio18
-def t_cursos(cc,tc):
-    t_cursos=(cc*tc)
-    return t_cursos
-#fin ejercicio18
+#EJERCICIO 18
+def calcular_distancia(ve,ti):
+    calcular_distancia=(ve*ti)
+    return  calcular_distancia
+#fin
 
-#ejercicio19
-def a_e(p,s):
-    a_e=(p+s)
-    return a_e
-#fin ejercicio19
+#EJERCICIO 19
+def calcular_velocidad(di,ti):
+    calcular_velocidad=(di/ti)
+    return  calcular_velocidad
+#fin
 
-#ejercicio20
-def t_p(pr,ti):
-    t_p=((pr*ti)+1)
-    return t_p
-#fin ejercicio20
+#EJERCICIO 20
+def area_trapecio(b1,b2,a):
+    area_trapecio=((b1+b2/2)*a)
+    return  area_trapecio
+#fin
 
-#ejercicio21
-def t_edad(c,s,h,ce,g):
-    t_edad=(c+s+h+ce+g)
-    return t_edad
-#fin ejercicio21
+#EJERCICIO 21
+def total_sobrinos(sbr,he):
+    total_sobrinos=(he*sbr)
+    return  total_sobrinos
+#fin
 
-#ejercicio22
-def g_v(pv,paq):
-    g_v=(pv*paq)
-    return g_v
-#fin ejercicio22
+#EJERCICIO 22
+def calcular_tiempo(di,ve):
+    calcular_tiempo=(di/ve)
+    return  calcular_tiempo
+#fin
 
-#ejercicio23
-def l_n(ul,tl):
-    l_n=(ul*tl)
-    return l_n
-#fin ejercicio23
+#EJERCICIO 23
+def calcular_presion(f,a):
+    calcular_presion=(f/a)
+    return  calcular_presion
 
-#ejercicio24
-def venta_desayuno(so,qu):
-    venta_desayuno=(so+qu)
-    return venta_desayuno
-#fin ejercicio24
+#EJERCICIO 24
+def area_cuadrado(l):
+    area_cuadrado=(l^2)
+    return  area_cuadrado
+#fin
 
-#ejercicio25
-def l_leche(nv,nl):
-    l_leche=(nv*nl)
-    return l_leche
-#fin ejercicio25
+#EJERCICIO 25
+def cervezas_vendidas(cvz,unid):
+    cervezas_vendidas=(cvz*unid)
+    return  cervezas_vendidas
+#fin
